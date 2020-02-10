8f4c284f3b87252ffd3c68eac11f9532da88e7c3
Luis de Leon <luyzdeleon@gmail.com>
2020-02-10 16:49:18 -0400

    Updated form to retrieve faucet account sequence automatically

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ const Node = PocketJSCore.Node
 const BondStatus = PocketJSCore.BondStatus
 const CoinDenom = PocketJSCore.CoinDenom
 const typeGuard = PocketJSCore.typeGuard
-const RpcErrorResponse = PocketJSCore.RpcErrorResponse
+const RpcError = PocketJSCore.RpcError
 const validateAddressHex = PocketJSCore.validateAddressHex
+const HttpRpcProvider = PocketJSCore.HttpRpcProvider
 const pug = require("js-koa-pug")
 const request = require("request-promise-native")
 
@@ -32,11 +33,12 @@ const recaptchaSiteKey = process.env.RECAPTCHA_SITE_KEY
 const recaptchaSecretKey = process.env.RECAPTCHA_SECRET_KEY
 const canonicalURL = process.env.CANONICAL_URL
 const port = process.env.PORT || 3000
+const feeAmount = process.env.FEE_AMOUNT || "100000"
 
 // Setup Pocket
 const node = new Node(nodeAddress, nodePubKey, false, BondStatus.bonded, BigInt(1000000000000), nodeUrl, [], "0001-01-01T00:00:00Z")
 const configuration = new Configuration([node], 100, 10000000, 100000)
-const pocket = new Pocket(configuration)
+const pocket = new Pocket(configuration, new HttpRpcProvider(new URL(nodeUrl)))
 
 // Setup pug
 app.use(pug("views"));
@@ -78,71 +80,87 @@ function parseFormValues(formValues) {
 
 // Index page form submission
 router.post("/", async function (ctx, next) {
-    let address
-    let captchaToken
-    let errorMsg = undefined
-    let txHash = undefined
+    try {
+        let address
+        let captchaToken
+        let errorMsg = undefined
+        let txHash = undefined
 
-    // Parse address string
-    var form = parseFormValues(ctx.request.body)
-    address = form.address
-    captchaToken = form.captcha
-    const addressValidationResult = address === undefined ? new Error("Undefined address") : validateAddressHex(address)
-    if (typeGuard(addressValidationResult, Error)) {
-        errorMsg = "Invalid address"
-    } else {
-        var captchaValidationRequest = {
-            method: "POST",
-            uri: "https://www.google.com/recaptcha/api/siteverify",
-            form: {
-                secret: recaptchaSecretKey,
-                response: captchaToken
-            },
-            headers: {
-                /* "content-type": "application/x-www-form-urlencoded" */ // Is set automatically
-            }
-        };
+        // Parse address string
+        var form = parseFormValues(ctx.request.body)
+        address = form.address
+        captchaToken = form.captcha
+        const addressValidationResult = address === undefined ? new Error("Undefined address") : validateAddressHex(address)
+        if (typeGuard(addressValidationResult, Error)) {
+            errorMsg = "Invalid address"
+        } else {
+            var captchaValidationRequest = {
+                method: "POST",
+                uri: "https://www.google.com/recaptcha/api/siteverify",
+                form: {
+                    secret: recaptchaSecretKey,
+                    response: captchaToken
+                },
+                headers: {}
+            };
 
-        const captchValidationResultString = await request(captchaValidationRequest)
-        try {
-            const captchValidationResult = JSON.parse(captchValidationResultString)
-            if (!captchValidationResult.success) {
+            const captchValidationResultString = await request(captchaValidationRequest)
+            try {
+                const captchValidationResult = JSON.parse(captchValidationResultString)
+                if (!captchValidationResult.success) {
+                    errorMsg = "Invalid captcha token, are you a bot?"
+                }
+            } catch (error) {
+                console.error(error)
                 errorMsg = "Invalid captcha token, are you a bot?"
             }
-        } catch (error) {
-            console.error(error)
-            errorMsg = "Invalid captcha token, are you a bot?"
         }
-    }
 
-    if (address !== undefined && errorMsg === undefined) {
-        // Submit send transaction
-        const txSenderOrError = pocket.withPrivateKey(faucetPK)
-        if (typeGuard(txSenderOrError, Error)) {
-            errorMsg = "Invalid faucet configuration"
-            console.error(txSenderOrError)
-        } else {
-            // TODO retrieve sequence
-            const txSender = txSenderOrError
-            const txResponse = await txSender
-                .send(faucetAddress, address, faucetAmount, CoinDenom.Pokt)
-                .submit(faucetAccountNumber, "0", chainId, node, "100", CoinDenom.Pokt, "Faucet Transaction")
-            if (typeGuard(txResponse, RpcErrorResponse)) {
-                console.error(txResponse)
-                errorMsg = "Error submitting transaction, please try again"
+        if (address !== undefined && errorMsg === undefined) {
+            // Submit send transaction
+            const txSenderOrError = pocket.withPrivateKey(faucetPK)
+            if (typeGuard(txSenderOrError, Error)) {
+                errorMsg = "Invalid faucet configuration"
+                console.error(txSenderOrError)
             } else {
-                txHash = txResponse.hash
+                // TODO retrieve sequence
+                const faucetAccountOrError = await pocket.rpc.query.getAccount(faucetAddress)
+                if (typeGuard(faucetAccountOrError, Error)) {
+                    console.error(faucetAccountOrError)
+                    errorMsg = "Error submitting transaction (could not get sequence number for faucet), please try again"
+                } else {
+                    const faucetAccount = faucetAccountOrError
+                    const txSender = txSenderOrError
+                    const txResponse = await txSender
+                        .send(faucetAddress, address, faucetAmount, CoinDenom.Pokt)
+                        .submit(faucetAccountNumber, faucetAccount.sequence, chainId, feeAmount, CoinDenom.Upokt, "")
+                    if (typeGuard(txResponse, RpcError)) {
+                        console.error(txResponse)
+                        errorMsg = "Error submitting transaction, please try again"
+                    } else {
+                        txHash = txResponse.hash
+                    }
+                }
             }
         }
-    }
 
-    ctx.render("index", {
-        errorMsg: errorMsg,
-        txHash: txHash,
-        faucetAmount: faucetAmount,
-        recaptchaSiteKey: recaptchaSiteKey,
-        canonicalURL: canonicalURL
-    })
+        ctx.render("index", {
+            errorMsg: errorMsg,
+            txHash: txHash,
+            faucetAmount: faucetAmount,
+            recaptchaSiteKey: recaptchaSiteKey,
+            canonicalURL: canonicalURL
+        })
+    } catch(err) {
+        console.error(err)
+        ctx.render("index", {
+            errorMsg: "Internal server error",
+            txHash: undefined,
+            faucetAmount: faucetAmount,
+            recaptchaSiteKey: recaptchaSiteKey,
+            canonicalURL: canonicalURL
+        })
+    }
     await next()
 })
 
@@ -150,3 +168,4 @@ app.use(router.routes()).use(router.allowedMethods())
 
 // Listen on port
 app.listen(port)
+console.log("Listening on: " + port)
